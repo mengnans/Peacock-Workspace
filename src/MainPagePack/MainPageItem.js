881c7b91fe5dad40a573647f2035d60547148442
Colin <rChansey5887@gmail.com>
2018-01-04 09:09:52 +1100

    Framework refractory

---
+++ src/MainPagePack/MainPageItem.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './MainPageItemStyle.css';
+import {Container, Row, Col} from 'reactstrap';
+
+import {SideBarItem} from './SideBarItem.js';
+import {NavigationItem} from './NavigationItem.js';
+import {SideChatPage} from '../SideChatPagePack/SideChatPage.js';
+
+export class MainPageItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isSideChatTurnedOn: false,
+            isSideBarFull: true,
+        };
+    }
+
+    render() {
+        return (
+            <Container fluid={true} className={"my-web"}>
+                <NavigationItem onClick={() => this.handleSideChatClick()}/>
+                <MyContainer isSideChatTurnedOn={this.state.isSideChatTurnedOn}
+                             isSideBarFull={this.state.isSideBarFull}
+                             onClick={() => this.handleSideBarClick()}/>
+            </Container>
+        );
+    }
+
+    handleSideChatClick() {
+        this.setState({
+            isSideChatTurnedOn: !this.state.isSideChatTurnedOn,
+        });
+    }
+
+    handleSideBarClick() {
+        this.setState({
+            isSideBarFull: !this.state.isSideBarFull,
+        });
+    }
+}
+
+
+
+function MyContainer(props) {
+    return (
+        <Container fluid={true} className={'my-container'}>
+            <SideBarItem isSideBarFull={props.isSideBarFull} onClick={props.onClick}/>
+            <MyContent isSideChatTurnedOn={props.isSideChatTurnedOn}
+                       isSideBarFull={props.isSideBarFull}/>
+            <SideChatPage isSideChatTurnedOn={props.isSideChatTurnedOn}/>
+        </Container>
+    );
+}
+
+class MyContent extends React.Component {
+    render() {
+        const sideBarClass = this.props.isSideBarFull ? 'my-content-with-side-bar-full' : 'my-content-without-side-bar-full';
+        const sideChatClass = this.props.isSideChatTurnedOn ? 'my-content-with-side-chat-on' : 'my-content-with-side-chat-off';
+        const classes = `my-content ${sideBarClass} ${sideChatClass}`;
+        return (
+            <Container id={"my-content"} fluid={true} className={classes}>
+                <Row>
+
+                </Row>
+            </Container>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,77 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import {Container, Row, Col} from 'reactstrap';
 
-import {SideBarItem} from './MainPagePack/SideBarItem.js';
-import {NavigationItem} from './MainPagePack/NavigationItem.js';
-import {SideChatPage} from './SideChatPagePack/SideChatPage.js';
-
-class Web extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            isSideChatTurnedOn: false,
-            isSideBarFull: true,
-        };
-    }
-
-    render() {
-        return (
-            <Container fluid={true} className={"my-web"}>
-                <NavigationItem onClick={() => this.handleSideChatClick()}/>
-                <MyContainer isSideChatTurnedOn={this.state.isSideChatTurnedOn}
-                             isSideBarFull={this.state.isSideBarFull}
-                             onClick={() => this.handleSideBarClick()}/>
-            </Container>
-        );
-    }
-
-    handleSideChatClick() {
-        this.setState({
-            isSideChatTurnedOn: !this.state.isSideChatTurnedOn,
-        });
-    }
-
-    handleSideBarClick() {
-        this.setState({
-            isSideBarFull: !this.state.isSideBarFull,
-        });
-    }
-}
-
-class MyContent extends React.Component {
-    render() {
-        const sideBarClass = this.props.isSideBarFull ? 'my-content-with-side-bar-full' : 'my-content-without-side-bar-full';
-        const sideChatClass = this.props.isSideChatTurnedOn ? 'my-content-with-side-chat-on' : 'my-content-with-side-chat-off';
-        const classes = `my-content ${sideBarClass} ${sideChatClass}`;
-        return (
-            <Container id={"my-content"} fluid={true} className={classes}>
-                <Row>
-
-                </Row>
-            </Container>
-        );
-    }
-}
-
-function MyContainer(props) {
-    return (
-        <Container fluid={true} className={'my-container'}>
-            <SideBarItem isSideBarFull={props.isSideBarFull} onClick={props.onClick}/>
-            <MyContent isSideChatTurnedOn={props.isSideChatTurnedOn}
-                       isSideBarFull={props.isSideBarFull}/>
-            <SideChatPage isSideChatTurnedOn={props.isSideChatTurnedOn}/>
-        </Container>
-    );
-}
-
-
-// ========================================
+import {MainPageItem} from './MainPagePack/MainPageItem.js';
 
 ReactDOM.render(
-    <Web/>,
+    <MainPageItem/>,
     document.getElementById('root')
 );
